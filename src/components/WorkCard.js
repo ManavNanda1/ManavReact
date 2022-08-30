66943f88049b0bc0eb533aee50836d395c0d1168
manav <nandamanav7@gmail.com>
2022-08-30 08:10:04 +0530

    add projects

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -5,16 +5,16 @@ import { NavLink } from "react-router-dom"
 const WorkCard = (props) => {
   return (
     <div className="project-card">
-         <img src={props.imgsrc} alt="image"/>
-          <h2 className="project-title">{props.title}</h2>
-            <div className="pro-details">
-             <p> {props.text}</p>
-             <div className="pro-btns"> 
-             <NavLink to="props.view" className="btn">View</NavLink>
-             <NavLink to="url.com" className="btn">Source</NavLink>
-             </div>
-             </div>
-            </div>
+      <img src={props.imgsrc} alt="image" />
+      <h2 className="project-title">{props.title}</h2>
+      <div className="pro-details">
+        <p> {props.text}</p>
+        <div className="pro-btns">
+          <a href="https://manavnanda.ml/myproject/" target="_blank" className="btn">View</a>
+          <a href="https://github.com/ManavNanda1/MyEcomWeb" target="_blank" className="btn">Source</a>
+        </div>
+      </div>
+    </div>
   )
 }
 
